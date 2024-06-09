ffc86cc94e0b0b0b56294ad87ae7091675f7e481
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-09 17:19:23 +0900

    feat: 회원가입, 비밀번호 변경 폼

---
+++ common/forms.py
@@ -0,0 +1,95 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
+
+from .models import User, UserManager
+
+
+class UserCreationForm(forms.ModelForm):
+    email = forms.EmailField(
+        label=_('Email'),
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': _('Email address'),
+                'required': 'True',
+            }
+        )
+    )
+    nickname = forms.CharField(
+        label=_('Nickname'),
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': _('Nickname'),
+                'required': 'True',
+            }
+        )
+    )
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': _('Password'),
+                'required': 'True',
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label=_('Password confirmation'),
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': _('Password confirmation'),
+                'required': 'True',
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'nickname')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('이미 사용 중인 이메일 주소입니다.')
+        return email
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if len(password1) < 8 or len(password1) > 20:
+            raise forms.ValidationError('비밀번호는 8자 이상 20자 이하이어야 합니다.')
+        return password1
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = UserManager.normalize_email(self.cleaned_data['email'])
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    # 비밀번호 변경 폼
+    password = ReadOnlyPasswordHashField(
+        label=_('Password')
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'is_active', 'is_superuser')
+
+    def clean_password(self):
+        return self.initial["password"]
