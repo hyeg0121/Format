86cb104d51832efb745092289578f6b0301ea327
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-09 21:45:18 +0900

    feat: 설문조사 만들기 폼 추가

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create_survey, name='create_survey'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from app.forms import SurveyForm
 
 
 def index(request):
     return render(request, 'app/index.html')
+
+
+def create_survey(request):
+    if request.method == 'POST':
+        form = SurveyForm(request.POST, request.FILES)
+        if form.is_valid():
+            survey = form.save(commit=False)
+            survey.user = request.user
+            survey.save()
+            return redirect('index')  # 설문조사 목록 페이지로 리디렉션
+    else:
+        form = SurveyForm()
+    return render(request, 'app/create_survey.html', {'form': form})
