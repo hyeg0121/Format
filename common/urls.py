c934c7351ea23f630785406ae5da9a4d0ecfb24f
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-09 21:40:55 +0900

    feat: EmailLoginView 적용

--- common/urls.py
+++ common/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from .views import EmailLoginView
 from . import views
 
 app_name = 'common'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('login/', EmailLoginView.as_view(), name='login'),
 ]
