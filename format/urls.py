dc1df837ed295e0148f2cda607b52e75973044dd
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-09 21:41:06 +0900

    feat: app url 추가

--- format/urls.py
+++ format/urls.py
@@ -1,8 +1,11 @@
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from format import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('common/', include('common.urls'), name='common'),
-]
+    path('', include('app.urls'), name='app'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
