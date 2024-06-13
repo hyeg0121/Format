8beb2a499f22f2778e0fa26cccfbc24d113aa80a
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-13 10:02:38 +0900

    feat: 설문조사 폼에 부트스트랩 적용

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,26 @@
+from datetime import datetime, timedelta
+
 from django import forms
 from .models import Survey, Question
 
 
 class SurveyForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # end_date 필드의 초기 값을 오늘 날짜에서 일주일 후로 설정
+        initial_end_date = datetime.now() + timedelta(days=7)
+        self.fields['end_date'].initial = initial_end_date.strftime('%Y-%m-%dT%H:%M')  # 날짜 형식에 맞게 포맷 지정
+
     class Meta:
         model = Survey
         fields = ['title', 'description', 'thumbnail_path', 'is_searchable', 'end_date']
         widgets = {
-            'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'thumbnail_path': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'is_searchable': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
-
 
 class QuestionForm(forms.ModelForm):
     class Meta:
