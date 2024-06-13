00376329329ff45d9d280c67977c7337ed39915c
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-13 10:02:47 +0900

    feat: STATIC_DIRS

--- format/settings.py
+++ format/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'common.User'
