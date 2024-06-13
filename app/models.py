584f3c559af8edaa8a413c6717534194b8e82c36
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-13 09:34:18 +0900

    feat: 문항 모델 추가

--- app/models.py
+++ app/models.py
@@ -14,8 +14,31 @@ class Survey(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='마지막 수정 날짜')
 
     def __str__(self):
-        return self.title
+        return f'name: {self.title}, description: {self.description}'
 
     class Meta:
         verbose_name = '설문조사'
         verbose_name_plural = '설문조사'
+
+
+class Question(models.Model):
+    SUBJECTIVE = 'SUBJECTIVE'
+    SINGLE_CHOICE = 'SINGLE_CHOICE'
+    QUESTION_TYPES = [
+        (SUBJECTIVE, '주관식'),
+        (SINGLE_CHOICE, '단일 선택')
+    ]
+
+    survey = models.ForeignKey('Survey', on_delete=models.CASCADE, related_name='questions', verbose_name='설문조사')
+    title = models.CharField(max_length=100, verbose_name='문항 제목')
+    question_type = models.CharField(max_length=15, choices=QUESTION_TYPES, verbose_name='문항 타입')
+    options = models.JSONField(null=True, blank=True, verbose_name='선택지 (다중 선택일 경우)')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성 날짜')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='마지막 수정 날짜')
+
+    def __str__(self):
+        return f"Survey ID: {self.survey_id}, Title: {self.title}, Question Type: {self.get_question_type_display()}"
+
+    class Meta:
+        verbose_name = '설문 문항'
+        verbose_name_plural = '설문 문항들'
