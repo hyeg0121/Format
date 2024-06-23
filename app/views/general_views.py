d5f45e136b5dac1adb59018d38138081cd3480ff
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-23 13:27:57 +0900

    feat: index 페이지 최근 설문 리스트 페이지네이션

--- app/views/general_views.py
+++ app/views/general_views.py
@@ -1,8 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-
 from app.models import Survey
 
 
 def index(request):
-    surveys = Survey.objects.all()  # 모든 설문조사 가져오기
-    return render(request, 'app/index.html', {'surveys': surveys})
+    surveys_list = Survey.objects.all()  # 모든 설문조사 가져오기
+
+    paginator = Paginator(surveys_list, 9)  # 페이지당 12개씩 분할
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'app/index.html', {'page_obj': page_obj})
