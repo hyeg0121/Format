b46676dfd658ee102c852a5a41c15989a367dc53
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-23 10:48:10 +0900

    refactor: 속성 변경
    pk -> id

--- app/views/response_views.py
+++ app/views/response_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+
 from ..models import Survey, Response
 
 
@@ -9,12 +10,13 @@ def survey_response(request, survey_id):
     if request.method == 'POST':
         responses = {}
         for question in survey.questions.all():
-            key = str(question.pk)
-            response = request.POST.get(key, '')  # 기본값으로 빈 문자열을 설정
+            key = str(question.id)
+            response = request.POST.get(key, '')
             responses[key] = response
 
         # 응답 저장
         Response.objects.create(user=request.user, survey=survey, responses=responses)
+
         return redirect('app:survey_detail', survey_id=survey_id)
 
     return render(request, 'app/page/response/survey_response.html', {'survey': survey})
