83dde029818397babff1adf54987ca83d9249ad9
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-23 10:47:24 +0900

    fix: 데이터 타입 변경
    models.JSONField -> djongo.models.JSONField

--- app/models/response_model.py
+++ app/models/response_model.py
@@ -1,11 +1,12 @@
+import djongo.models
 from django.db import models
 from django.conf import settings
 
 
 class Response(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='응답자')
-    survey = models.ForeignKey('app.Survey', on_delete=models.CASCADE, related_name='answers', verbose_name='설문조사')
-    responses = models.JSONField(default=list, verbose_name='응답')
+    survey = models.ForeignKey('app.Survey', on_delete=models.CASCADE, related_name='responses', verbose_name='설문조사')
+    responses = djongo.models.JSONField(default=dict, verbose_name='응답')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성 날짜')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='마지막 수정 날짜')
 
@@ -15,6 +16,7 @@ class Response(models.Model):
     class Meta:
         verbose_name = '응답'
         verbose_name_plural = '응답'
+
 
 
 
