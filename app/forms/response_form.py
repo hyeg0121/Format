886a0996181398fedd79367e6da79bf2294aed77
hyeg0121 <w2218@e-mirim.hs.kr>
2024-06-23 11:02:12 +0900

    refactor: SurveyResponseForm 이용

--- app/forms/response_form.py
+++ app/forms/response_form.py
@@ -1,5 +1,24 @@
+import json
+
 from django import forms
 
 
 class SurveyResponseForm(forms.Form):
-    pass
+    def __init__(self, *args, **kwargs):
+        survey = kwargs.pop('survey')
+        super().__init__(*args, **kwargs)
+
+        for question in survey.questions.all():
+            if question.question_type == 'SINGLE_CHOICE':
+                options = json.loads(question.options)
+                self.fields[str(question.id)] = forms.ChoiceField(
+                    choices=[(option, option) for option in options],
+                    widget=forms.RadioSelect,
+                    label=question.title
+                )
+            elif question.question_type == 'SUBJECTIVE':
+                self.fields[str(question.id)] = forms.CharField(
+                    widget=forms.Textarea,
+                    required=True,
+                    label=question.title
+                )
--- app/views/response_views.py
+++ app/views/response_views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+from ..forms import SurveyResponseForm
 from ..models import Survey, Response
 
 
 @login_required
 def survey_response(request, survey_id):
     survey = get_object_or_404(Survey, id=survey_id)
+
     if request.method == 'POST':
-        responses = {}
-        for question in survey.questions.all():
-            key = str(question.id)
-            response = request.POST.get(key, '')
-            responses[key] = response
+        form = SurveyResponseForm(request.POST, survey=survey)
+        if form.is_valid():
+            responses = form.cleaned_data
+            response_obj = Response.objects.create(user=request.user, survey=survey, responses=responses)
+            return redirect('app:survey_detail', survey_id=survey.id)
+    else:
+        form = SurveyResponseForm(survey=survey)
 
-        # 응답 저장
-        Response.objects.create(user=request.user, survey=survey, responses=responses)
-
-        return redirect('app:survey_detail', survey_id=survey_id)
-
-    return render(request, 'app/page/response/survey_response.html', {'survey': survey})
+    return render(request, 'app/page/response/survey_response.html', {'survey': survey, 'form': form})
 
 
